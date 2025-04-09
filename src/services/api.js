5b6a5049fce7ada49eb3453baf074498a9dac38b
Yellathkota-Jaswanth <jaswanthyellathkota@gmail.com>
2025-04-09 17:00:10 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: 'https://inventory-72zv.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
